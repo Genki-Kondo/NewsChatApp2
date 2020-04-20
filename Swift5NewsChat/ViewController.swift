3d0f09f145af9494ea6ea20d4bb121c68a42894d
近藤元気 <genki7onepiece@gmail.com>
2020-04-20 17:12:08 +0900

    Initial Commit

---
+++ Swift5NewsChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5NewsChat
+//
+//  Created by 近藤元気 on 2020/04/20.
+//  Copyright © 2020 genkikondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
